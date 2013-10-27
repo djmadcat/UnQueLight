5f45c28b139825419f9cf31c3598995579e4d606
Alexey Aleshkov <djmadcat@gmail.com>
2013-10-27 22:42:53 +0400

    Add NSException names

---
+++ UnQueLight/UQLException.h
@@ -0,0 +1,18 @@
+//
+//  UQLException.h
+//  UnQueLight
+//
+//  Created by Alexey Aleshkov on 27.10.13.
+//  Copyright (c) 2013 Alexey Aleshkov. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString *const UQLOutOfMemoryException;
+extern NSString *const UQLReadOnlyException;
+extern NSString *const UQLNotImplementedException;
+extern NSString *const UQLAccessDeniedException;
+extern NSString *const UQLIOException;
+extern NSString *const UQLVirtualMachineException;
---
+++ UnQueLight/UQLException.m
@@ -0,0 +1,18 @@
+//
+//  UQLException.m
+//  UnQueLight
+//
+//  Created by Alexey Aleshkov on 27.10.13.
+//  Copyright (c) 2013 Alexey Aleshkov. All rights reserved.
+//
+
+
+#import "UQLException.h"
+
+
+NSString *const UQLOutOfMemoryException = @"UQLOutOfMemoryException";
+NSString *const UQLReadOnlyException = @"UQLReadOnlyException";
+NSString *const UQLNotImplementedException = @"UQLNotImplementedException";
+NSString *const UQLAccessDeniedException = @"UQLAccessDeniedException";
+NSString *const UQLIOException = @"UQLIOException";
+NSString *const UQLVirtualMachineException = @"UQLVirtualMachineException";
