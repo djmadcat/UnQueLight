b50998f1fef1877de77fef89e3c29e934777a8cb
Alexey Aleshkov <djmadcat@gmail.com>
2013-10-27 16:50:10 +0400

    Add simple database tests

--- Tests/Tests/UQLDatabaseTests.m
+++ Tests/Tests/UQLDatabaseTests.m
@@ -11,10 +11,83 @@
 
 
 @interface UQLDatabaseTests : UQLTestCase
-
+@property (nonatomic, copy) NSString *databasePath;
 @end
 
 
 @implementation UQLDatabaseTests
+
+- (void)setUp
+{
+	[super setUp];
+
+	self.databasePath = [self tempDatabasePathForFileName:@"testDatabase"];
+}
+
+- (void)testCreateFileDatabase
+{
+	UQLDatabase *database = [[UQLDatabase alloc] initWithPath:self.databasePath];
+	NSError *error = nil;
+
+	BOOL opened = [database openWithOptions:UQLOpenCreate error:&error];
+	STAssertTrue(opened, @"Can not create database at path \"%@\"", self.databasePath);
+
+	BOOL closed = [database close];
+	STAssertTrue(closed, @"Can not close database at path \"%@\"", self.databasePath);
+}
+
+- (void)testOpenFileDatabase
+{
+	UQLDatabase *database = [[UQLDatabase alloc] initWithPath:self.databasePath];
+	NSError *error = nil;
+
+	BOOL opened = [database openWithOptions:UQLOpenReadWrite error:&error];
+	STAssertTrue(opened, @"Can not open database at path \"%@\"", self.databasePath);
+
+	BOOL closed = [database close];
+	STAssertTrue(closed, @"Can not close database at path \"%@\"", self.databasePath);
+}
+
+- (void)testInMemoryDatabase
+{
+	UQLDatabase *database = [[UQLDatabase alloc] init];
+	NSError *error = nil;
+
+	BOOL opened = [database openWithOptions:UQLOpenReadWrite error:&error];
+	STAssertTrue(opened, @"Can not open in-memory database");
+
+	BOOL closed = [database close];
+	STAssertTrue(closed, @"Can not close in-memory database");
+}
+
+- (void)testOpenTwiceDatabase
+{
+	UQLDatabase *database = [[UQLDatabase alloc] init];
+	NSError *error = nil;
+
+	BOOL opened = [database openWithOptions:UQLOpenReadWrite error:&error];
+	STAssertTrue(opened, @"Can not open database");
+
+	BOOL openedTwice = [database openWithOptions:UQLOpenReadWrite error:&error];
+	STAssertTrue(openedTwice, @"Can not open database twice");
+
+	BOOL closed = [database close];
+	STAssertTrue(closed, @"Can not close database");
+}
+
+- (void)testCloseTwiceDatabase
+{
+	UQLDatabase *database = [[UQLDatabase alloc] init];
+	NSError *error = nil;
+
+	BOOL opened = [database openWithOptions:UQLOpenReadWrite error:&error];
+	STAssertTrue(opened, @"Can not open database");
+
+	BOOL closed = [database close];
+	STAssertTrue(closed, @"Can not close database");
+
+	BOOL closedTwice = [database close];
+	STAssertTrue(closedTwice, @"Can not close database twice");
+}
 
 @end
