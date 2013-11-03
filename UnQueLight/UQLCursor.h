e9f7c410dcb33abca59856357f5f4b0e6b3508f8
Alexey Aleshkov <djmadcat@gmail.com>
2013-11-03 14:24:24 +0400

    Add cursor

---
+++ UnQueLight/UQLCursor.h
@@ -0,0 +1,50 @@
+//
+//  UQLCursor.h
+//  UnQueLight
+//
+//  Created by Alexey Aleshkov on 06.10.13.
+//  Copyright (c) 2013 Alexey Aleshkov. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+#include "unqlite.h"
+#import "UQLDatabase.h"
+
+
+typedef NS_ENUM(NSUInteger, UQLCursorSeekMatch) {
+	UQLCursorSeekMatchExact				= UNQLITE_CURSOR_MATCH_EXACT,
+	UQLCursorSeekMatchLessOrEqual		= UNQLITE_CURSOR_MATCH_LE,
+	UQLCursorSeekMatchGreaterOrEqual	= UNQLITE_CURSOR_MATCH_GE
+};
+
+
+@class UQLDatabase;
+
+@interface UQLCursor : NSObject
+{
+	unqlite_kv_cursor *_cursor;
+}
+
+@property (nonatomic, strong, readonly) UQLDatabase *database;
+
+- (id)initWithDatabase:(UQLDatabase *)database error:(NSError **)error;
+
+- (void)reset;
+
+- (BOOL)isValid;
+- (BOOL)seekForRawKey:(NSData *)key match:(UQLCursorSeekMatch)match;
+- (BOOL)first;
+- (BOOL)last;
+- (BOOL)next;
+- (BOOL)previous;
+
+- (BOOL)remove;
+
+- (NSData *)rawKey;
+- (NSData *)rawValue;
+
+// low-level handle
+- (unqlite_kv_cursor *)unqliteHandle;
+
+@end
---
+++ UnQueLight/UQLCursor.m
@@ -0,0 +1,192 @@
+//
+//  UQLCursor.m
+//  UnQueLight
+//
+//  Created by Alexey Aleshkov on 06.10.13.
+//  Copyright (c) 2013 Alexey Aleshkov. All rights reserved.
+//
+
+
+#import "UQLCursor.h"
+#import "UQLError.h"
+#import "UQLLog.h"
+
+
+@interface UQLCursor ()
+@property (nonatomic, strong, readwrite) UQLDatabase *database;
+@end
+
+
+@implementation UQLCursor
+
+#pragma mark -
+#pragma mark Init / Dealloc
+
+- (id)initWithDatabase:(UQLDatabase *)database error:(NSError **)error
+{
+	UQLLogTrace(@"Creating cursor for database \"%@\"", database.path);
+
+	if (!database) {
+		UQLLogDebug(@"Failed to create cursor for NULL database");
+		return nil;
+	}
+
+	self = [super init];
+	if (!self) {
+		return nil;
+	}
+
+	self.database = database;
+
+	unqlite *db = [self.database unqliteHandle];
+	int status = unqlite_kv_cursor_init(db, &_cursor);
+	if (status != UNQLITE_OK) {
+		NSError *initError = nil;
+		if (status == UNQLITE_CORRUPT) {
+			initError = [NSError errorWithDomain:UQLErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Corrupted database pointer" }];
+		} else if (status == UNQLITE_ABORT) {
+			initError = [NSError errorWithDomain:UQLErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Another thread have released the database handle" }];
+		} else if (status == UNQLITE_NOTIMPLEMENTED) {
+			initError = [NSError errorWithDomain:UQLErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Storage engine does not support cursors" }];
+		} else if (status == UNQLITE_NOMEM) {
+			initError = [NSError errorWithDomain:UQLErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Out of memory" }];
+		}
+		NSAssert(initError != nil, @"Unhandled error returned: %d", status);
+
+		UQLLogError(@"Cursor for database \"%@\" can not be created. Error returned %@", self.database.path, initError);
+		if (error) {
+			*error = initError;
+		}
+
+		return nil;
+	}
+
+	NSAssert(_cursor != NULL, @"Something going wrong! ivar can not be NULL");
+
+	UQLLogTrace(@"Successful created cursor for database \"%@\"", self.database.path);
+
+	return self;
+}
+
+- (void)dealloc
+{
+	UQLLogTrace(@"Trying to release cursor for database \"%@\"", self.database.path);
+
+	NSAssert(_cursor != NULL, @"Something going wrong! ivar can not be NULL");
+
+	int status = unqlite_kv_cursor_release([self.database unqliteHandle], _cursor);
+	if (status != UNQLITE_OK) {
+		NSError *deallocError = nil;
+		if (status == UNQLITE_CORRUPT) {
+			deallocError = [NSError errorWithDomain:UQLErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Another thread have released the database or cursor handle" }];
+		} else if (status == UNQLITE_ABORT) {
+			deallocError = [NSError errorWithDomain:UQLErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Corrupted database or cursor pointer" }];
+		}
+		NSAssert(deallocError != nil, @"Unhandled error returned");
+
+		UQLLogWarning(@"Cursor for database \"%@\" can not be released properly. Error returned %@", self.database.path, deallocError);
+	} else {
+		UQLLogTrace(@"Successful released cursor for database \"%@\"", self.database.path);
+	}
+
+	_cursor = NULL;
+}
+
+- (void)reset
+{
+	int status = unqlite_kv_cursor_reset(_cursor);
+	NSAssert(status != UNQLITE_CORRUPT, @"Cursor pointer is corrupted");
+}
+
+#pragma mark -
+#pragma mark Positioning
+
+- (BOOL)isValid
+{
+	BOOL result = unqlite_kv_cursor_valid_entry(_cursor);
+	return result;
+}
+
+- (BOOL)seekForRawKey:(NSData *)key match:(UQLCursorSeekMatch)match
+{
+	int status = unqlite_kv_cursor_seek(_cursor, [key bytes], (int)[key length], match);
+	return status == UNQLITE_OK;
+}
+
+- (BOOL)first
+{
+	int status = unqlite_kv_cursor_first_entry(_cursor);
+	return status == UNQLITE_OK;
+}
+
+- (BOOL)last
+{
+	int status = unqlite_kv_cursor_last_entry(_cursor);
+	return status == UNQLITE_OK;
+}
+
+- (BOOL)next
+{
+	int status = unqlite_kv_cursor_next_entry(_cursor);
+	return status == UNQLITE_OK;
+}
+
+- (BOOL)previous
+{
+	int status = unqlite_kv_cursor_prev_entry(_cursor);
+	return status == UNQLITE_OK;
+}
+
+#pragma mark -
+#pragma mark Deleting records
+
+- (BOOL)remove
+{
+	int status = unqlite_kv_cursor_delete_entry(_cursor);
+	return status == UNQLITE_OK;
+}
+
+#pragma mark -
+#pragma mark Extracting data
+
+- (NSData *)rawKey
+{
+	int size = 0;
+	int status = unqlite_kv_cursor_key(_cursor, NULL, &size);
+	if (status != UNQLITE_OK) {
+		return nil;
+	}
+	NSMutableData *result = [NSMutableData dataWithLength:size];
+	status = unqlite_kv_cursor_key(_cursor, [result mutableBytes], &size);
+	if (status != UNQLITE_OK) {
+		return nil;
+	}
+
+	return result;
+}
+
+- (NSData *)rawValue
+{
+	unqlite_int64 size = 0;
+	int status = unqlite_kv_cursor_data(_cursor, NULL, &size);
+	if (status != UNQLITE_OK) {
+		return nil;
+	}
+	NSMutableData *result = [NSMutableData dataWithLength:size];
+	status = unqlite_kv_cursor_data(_cursor, [result mutableBytes], &size);
+	if (status != UNQLITE_OK) {
+		return nil;
+	}
+
+	return result;
+}
+
+#pragma mark -
+#pragma mark Low-level methods
+
+- (unqlite_kv_cursor *)unqliteHandle
+{
+	return _cursor;
+}
+
+@end
